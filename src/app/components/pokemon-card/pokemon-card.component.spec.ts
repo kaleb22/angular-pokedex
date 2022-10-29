2e78b6e0f85795d3ddd90f96c1effea70b6bda23
kaleb22 <kaleb22@hotmail.com>
2022-10-29 12:04:40 -0300

    feat: impl test

--- src/app/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/components/pokemon-card/pokemon-card.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FullEntryNumberPipe } from 'src/app/pipes/full-entry-number.pipe';
+import { PokemonImgPipe } from 'src/app/pipes/pokemon-img.pipe';
 
 import { PokemonCardComponent } from './pokemon-card.component';
 
@@ -8,16 +10,21 @@ describe('PokemonCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PokemonCardComponent ]
+      declarations: [ PokemonCardComponent, FullEntryNumberPipe, PokemonImgPipe ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PokemonCardComponent);
     component = fixture.componentInstance;
+    component.entryNumber = 24;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have poison type background color', () => {
+    expect(component.bgGColor).toMatch('#A43E9E');
+  })
 });
