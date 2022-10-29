fc5ee2762da9338670e3ffe8b87d4eb01baef657
kaleb22 <kaleb22@hotmail.com>
2022-10-29 12:35:05 -0300

    feat: test

--- src/app/pipes/format-stat.pipe.spec.ts
+++ src/app/pipes/format-stat.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { pipe } from 'rxjs';
 import { FormatStatPipe } from './format-stat.pipe';
 
 describe('FormatStatPipe', () => {
+  const pipe = new FormatStatPipe();
   it('create an instance', () => {
-    const pipe = new FormatStatPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should format stat number between 0 and 9', () => {
+    let formatedNumber = pipe.transform(1);
+    expect(formatedNumber).toMatch('001');
+  });
+
+  it('should format stat number between 10 and 99', () => {
+    let formatedNumber = pipe.transform(52);
+    expect(formatedNumber).toMatch('052');
   });
 });
