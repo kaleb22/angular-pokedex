a1d100d71878e1e22466ea5712f2e54b4c5c8dd6
kaleb22 <kaleb22@hotmail.com>
2022-10-29 12:30:21 -0300

    feat: mocking activatedRoute class

--- src/app/components/pokemon-details/pokemon-details.component.spec.ts
+++ src/app/components/pokemon-details/pokemon-details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { PokemonDetailsComponent } from './pokemon-details.component';
 
@@ -6,9 +8,24 @@ describe('PokemonDetailsComponent', () => {
   let component: PokemonDetailsComponent;
   let fixture: ComponentFixture<PokemonDetailsComponent>;
 
+  class ActivatedRouteMock {
+    queryParams = new Observable( observer => {
+      const urlParams = {
+        entryNumber: 1
+      }
+
+      observer.next(urlParams);
+      observer.complete();
+    });
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PokemonDetailsComponent ]
+      declarations: [ PokemonDetailsComponent ],
+      providers: [{
+        provide: ActivatedRoute,
+        useClass: ActivatedRouteMock
+      }]
     })
     .compileComponents();
 
