f6469dcad41377e226289edfa84d3147f4bf64c4
kaleb22 <kaleb22@hotmail.com>
2022-10-29 12:23:17 -0300

    feat: test

--- src/app/components/stat/stat.component.spec.ts
+++ src/app/components/stat/stat.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokemonStat } from 'pokenode-ts';
+import { FormatStatPipe } from 'src/app/pipes/format-stat.pipe';
 
 import { StatComponent } from './stat.component';
 
@@ -6,14 +8,24 @@ describe('StatComponent', () => {
   let component: StatComponent;
   let fixture: ComponentFixture<StatComponent>;
 
+  const pokemonStatMock: PokemonStat = {
+    stat: {
+      name: 'hp',
+      url: ''
+    },
+    base_stat: 90,
+    effort: 30
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatComponent ]
+      declarations: [ StatComponent, FormatStatPipe ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(StatComponent);
     component = fixture.componentInstance;
+    component.stats = pokemonStatMock;
     fixture.detectChanges();
   });
 
