d9184c527bf279ee8e2d093581fc6e337f7d8fe0
kaleb22 <kaleb22@hotmail.com>
2022-10-29 12:38:29 -0300

    feat: test

--- src/app/pipes/full-entry-number.pipe.spec.ts
+++ src/app/pipes/full-entry-number.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { FullEntryNumberPipe } from './full-entry-number.pipe';
 
 describe('FullEntryNumberPipe', () => {
+  const pipe = new FullEntryNumberPipe();
+
   it('create an instance', () => {
-    const pipe = new FullEntryNumberPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should format entryNumber between 0 and 9', () => {
+    const formatedNumber = pipe.transform(2);
+    expect(formatedNumber).toMatch('#002');
+  });
+
+  it('should format entryNumber between 10 and 99', () => {
+    const formatedNumber = pipe.transform(99);
+    expect(formatedNumber).toMatch('#099');
+  });
+
+  it('should format entryNumber greater than 99', () => {
+    const formatedNumber = pipe.transform(100);
+    expect(formatedNumber).toMatch('#100');
   });
 });
